a425d5da977fbd0d6d02485422535da20e218491
Yasitha Bhanuka <127284944+Yasitha-Bhanuka@users.noreply.github.com>
2023-07-07 16:49:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ let username; // declare the vaiable first
 
 username = window.prompt("What is your name ?"); // using window.prompt we can get the input from the user by promting box.
 
-console.log(username); // go to the web page rigth click the page go to the inspect of page, you can see the console. 
+console.log(username); // go to the web page right click the page go to the inspect of page, you can see the console. 
 
-//after enter the name and click the ok buttton you can see which is you input in consoler.
+//after enter the name and click the ok buttton you can see which are you input in consoler.
